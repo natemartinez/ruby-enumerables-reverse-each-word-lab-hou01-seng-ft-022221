ff0b0d86baecc59e48179c2e9f00d491284effd4
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-02-14 20:07:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
 string_to_array = string.split(" ")
 sentence_array
-string_to_array.collect{ |element|
+string_to_array.collect{ |element| 
+element.reverse 
  
 end
   
